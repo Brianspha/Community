49cc67680781a6807695b98735e95b8b9c841bbf
dapps4africa <dapps4africa@yahoo.com>
2020-09-26 23:49:19 +0200

    update

--- packages/communitydapp/src/App.vue
+++ packages/communitydapp/src/App.vue
@@ -26,7 +26,33 @@
                                 <v-card-text class="text--primary">
                                     <div>{{ report.title }}</div>
                                     <div>{{ report.description }}</div>
-                                    <v-switch :v-model="report.resolved" label="Resolved" disabled></v-switch>
+                                    <v-switch value :input-value="report.resolved" label="Resolved" readonly></v-switch>
+                                </v-card-text>
+                                <v-card-actions>
+                                    <v-btn color="orange" @click="goto(report.receipt)" text>View Receipt</v-btn>
+                                    <v-btn v-if="!report.resolved"  color="orange" @click="openResolve(report)" text>Resolve</v-btn>
+                                    <v-btn color="orange" @click="openMaps(report.location)" text>View on Map</v-btn>
+                                </v-card-actions>
+                            </v-card>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-tab-item>
+            <v-tab-item key="1">
+                <v-container fluid>
+                    <v-row>
+                        <v-col v-for="(report, i) in $store.state.resolved" :key="i" cols="12" md="4">
+                            <v-card class="mx-auto" max-width="400">
+                                <v-img lazy-src="https://picsum.photos/id/11/100/60" aspect-ratio="1" class="grey lighten-2" max-width="500" max-height="300" :src="report.src"><template v-slot:placeholder>
+                                        <v-row class="fill-height ma-0" align="center" justify="center">
+                                            <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+                                        </v-row>
+                                    </template>
+                                </v-img>
+                                <v-card-text class="text--primary">
+                                    <div>{{ report.title }}</div>
+                                    <div>{{ report.description }}</div>
+                                    <v-switch value :input-value="report.resolved" label="Resolved" readonly></v-switch>
                                 </v-card-text>
                                 <v-card-actions>
                                     <v-btn color="orange" @click="goto(report.receipt)" text>View Receipt</v-btn>
@@ -91,6 +117,7 @@ export default {
         loadReports: async function () {
             this.isLoading = true;
             this.$store.state.reports = [];
+            this.$store.state.resolved = [];
             var reportedKeys = await this.communityContract.methods
                 .getReportKeys()
                 .call();
@@ -102,7 +129,8 @@ export default {
                 console.log("`${details[0]}`: ", details);
                 var resolvedReceipt= await this.communityContract.methods
                     .getResolvedReceipt(key).call()
-                this.$store.state.reports.push({
+                    if(details[6]){
+this.$store.state.resolved.push({
                     title: details[1],
                     src: details[0],
                     description: details[2],
@@ -112,6 +140,20 @@ export default {
                     resolvedLink: details[3],
                     resolvedReceipt: resolvedReceipt
                 });
+                    }
+                    else{
+this.$store.state.reports.push({
+                    title: details[1],
+                    src: details[0],
+                    description: details[2],
+                    receipt: details[4],
+                    resolved: details[6],
+                    location: JSON.parse(details[5]),
+                    resolvedLink: details[3],
+                    resolvedReceipt: resolvedReceipt
+                });
+                    }
+                
             });
             this.isLoading = false;
         }
